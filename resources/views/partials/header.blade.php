d49d8b369016c14226d40f3820c20af81dd660e6
mikzanco <mikyzancolo97@gmail.com>
2022-12-22 16:48:06 +0100

    create header

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,49 @@
+<header>
+
+    <div class="container">
+
+      <div>
+        <img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="Logo DC Comics">
+      </div>
+
+      <nav>
+
+        <ul>
+          <li>
+            <a class="{{ Route::currentRouteName() ===  'Character' ? 'active' : ' '}}" href="{{ route('home') }}">Character</a>
+          </li>
+          <li>
+            <a class="{{ Route::currentRouteName() ===  'Comics' ? 'active' : ' '}}" href="{{ route('home') }}">Comics</a>
+          </li>
+          <li>
+            <a class="{{ Route::currentRouteName() ===  'TV' ? 'active' : ' '}}" href="{{ route('home') }}">TV</a>
+          </li>
+          <li>
+            <a class="{{ Route::currentRouteName() ===  'Movies' ? 'active' : ' '}}" href="{{ route('home') }}">Movies</a>
+          </li>
+          <li>
+            <a class="{{ Route::currentRouteName() ===  'Games' ? 'active' : ' '}}" href="{{ route('home') }}">Games</a>
+          </li>
+          <li>
+            <a class="{{ Route::currentRouteName() ===  'Collectibles' ? 'active' : ' '}}" href="{{ route('home') }}">Collectibles</a>
+          </li>
+          <li>
+            <a class="{{ Route::currentRouteName() ===  'Videos' ? 'active' : ' '}}" href="{{ route('home') }}">Videos</a>
+          </li>
+          <li>
+            <a class="{{ Route::currentRouteName() ===  'Fans' ? 'active' : ' '}}" href="{{ route('home') }}">Fans</a>
+          </li>
+          <li>
+            <a class="{{ Route::currentRouteName() ===  'News' ? 'active' : ' '}}" href="{{ route('home') }}">News</a>
+          </li>
+          <li>
+            <a class="{{ Route::currentRouteName() ===  'Shop' ? 'active' : ' '}}" href="{{ route('home') }}">Shop</a>
+          </li>
+
+        </ul>
+
+      </nav>
+
+    </div>
+
+</header>
